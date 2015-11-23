e83976b0bdec44aa1d871c11c5178169bd46445a
laril-2 <laril@viidakko.fi>
2015-11-23 11:12:37 +0200

    sample program

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LianaMailerClient
+{
+	// Sample program to communicate with LM API endpoint "echoMessage"
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Client client = new Client("<REALM>", "<USER ID>", "<SECRET KEY>");
+
+			EchoResponse response = client.EchoMessage("echo this");
+			
+			Console.WriteLine("succeed: " + response.Succeed);
+			Console.WriteLine("result: " + response.Result);
+		}
+	}
+}
